97bce3a0cfd435eba24808f25dda5e4542d2a106
divya-gariga <divya.gariga@zemosolabs.com>
2023-07-19 16:45:11 +0530

    initial commit

---
+++ src/main/java/com/example/helloworld/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.example.helloworld.controller;
+
+import com.example.helloworld.util.SHA1HashGenerator;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+@RestController
+public class GreetingController {
+
+    @GetMapping("/")
+    public String index() {
+        return "<h1>Hello World!<h1>";
+    }
+}
